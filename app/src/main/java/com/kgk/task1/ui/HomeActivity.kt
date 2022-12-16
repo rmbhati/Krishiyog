d9c522939ee5e66cc631ca8b197008ef657b7205
rmbhati <ramesh1991>
2022-12-16 15:28:44 +0530

    cashing data

--- app/src/main/java/com/kgk/task1/ui/HomeActivity.kt
+++ app/src/main/java/com/kgk/task1/ui/HomeActivity.kt
@@ -4,17 +4,25 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.kgk.task1.R
 import com.kgk.task1.base.BaseActivity
 import com.kgk.task1.databinding.ActivityMainBinding
 import com.kgk.task1.utils.CustomProgress
+import com.kgk.task1.utils.PrefKeys
 import com.kgk.task1.utils.toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import java.lang.reflect.Type;
+import kotlin.collections.ArrayList
 
 class HomeActivity : BaseActivity() {
     private lateinit var activityBinding: ActivityMainBinding
     private val viewModel by viewModel<HomeViewModel>()
+    private val sdf = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
     private var dialog: CustomProgress? = null
 
     override fun initView() {
@@ -32,9 +40,32 @@ class HomeActivity : BaseActivity() {
         val did = DividerItemDecoration(recyclerView.context, layoutManager.orientation)
         recyclerView.addItemDecoration(did)
 
-        viewModel.getListData(true)
+        if (sharedPreferences.getString(PrefKeys.datetime, "") == "") {
+            viewModel.getListData(true)//if date and time is not saved in cashing
+        } else {
+            //date and time saved in cashing
+            val currentDate = sdf.parse(sdf.format(Date()))
+            val savedDate = sdf.parse(sharedPreferences.getString(PrefKeys.datetime, ""))
+            val diff: Long = currentDate.time - savedDate.time
+            val seconds = diff / 1000
+            val minutes = seconds / 60
+            val hours = minutes / 60
+            if (minutes > 120) {
+                viewModel.getListData(true)
+            } else {
+                val listType: Type = object : TypeToken<ArrayList<ListData?>?>() {}.type
+                val response: List<ListData> =
+                    Gson().fromJson(sharedPreferences.getString(PrefKeys.listData, ""), listType)
+                setAdapter(response)
+            }
+        }
         refresh.setOnRefreshListener { viewModel.getListData(false) }
         retry.setOnClickListener { viewModel.getListData(true) }
+    }
+
+    private fun setAdapter(response: List<ListData>) {
+        val adapter = HomeAdapter(this, response)
+        recyclerView.adapter = adapter
     }
 
     private fun showLoading(status: Boolean) {
@@ -47,9 +78,11 @@ class HomeActivity : BaseActivity() {
 
     private fun onSuccess(response: List<ListData>) {
         retry.visibility = View.GONE
-        val adapter = HomeAdapter(this, response)
-        recyclerView.adapter = adapter
+        setAdapter(response)
         refresh.isRefreshing = false
+        val currentDate = sdf.format(Date())
+        sharedPreferences.edit().putString(PrefKeys.listData, Gson().toJson(response)).apply()
+        sharedPreferences.edit().putString(PrefKeys.datetime, currentDate).apply()
     }
 
     private fun onFailed(error: String) {
--- app/src/main/java/com/kgk/task1/utils/PrefKeys.kt
+++ app/src/main/java/com/kgk/task1/utils/PrefKeys.kt
@@ -2,8 +2,8 @@ package com.kgk.task1.utils
 
 class PrefKeys {
     companion object {
-        const val isLogin = "isLogin"
-        const val loginId = "loginId"
+        const val listData = "listData"
+        const val datetime = "datetime"
     }
 }
 
